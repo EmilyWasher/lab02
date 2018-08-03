1313fb6848456af95e1ab2668935eb7ad67455c0
Emily Washer <z5061368@student.unsw.edu.au>
2018-08-03 15:41:12 +1000

    Added lines to all except 2nd smallest and bonus question

---
+++ number_list.py
@@ -0,0 +1,37 @@
+def find_reverse(numbers):
+    reverse = numbers[::-1]
+    pass
+
+def find_max(numbers):
+    maximum = max(numbers)
+    pass
+
+def find_min(numbers):
+    minimum = min(numbers)
+    pass
+
+def find_sum(numbers):
+    sum(numbers)
+    pass
+
+def find_average(numbers):
+    average = sum(numbers)/len(numbers)
+    pass
+
+def find_descending(numbers):
+    numbers.sort(reverse=True)
+    pass
+
+def second_smallest(numbers):
+    #TODO: find the second smallest
+    pass
+
+
+'''
+ bonus task:
+ a function that takes in a list of numbers, and an index 'k' 
+ and prints the kth smallest number in the list
+'''
+def kth_smallest(numbers, k):
+    #TODO: find the kth smallest number in the list
+    pass
